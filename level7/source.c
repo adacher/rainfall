5732853b5ecfa4dcdc14ae85dc905e8444f551a4
syboeuf <syboeuf@42.student.fr>
2020-05-23 00:41:15 +0200

    add level7

---
+++ level7/source.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void m (void) {
+    printf(" - %ld", time(NULL));
+}
+
+int main (int argc, char **argv) {
+    char *one;
+    char *two;
+    char *three;
+    char *four;
+    one = malloc(20);
+    two = malloc(20);
+    three = malloc(20);
+    four = malloc(20);
+    one = strcpy(one, argv[1]);
+    two = strcpy(two, argv[2]);
+    fopen("/home/user/level8/.pass", "r");
+    puts("~~");
+    return 0;
+}
