a95a26f6cd533fa42972bae419bba0966dc528db
syboeuf <syboeuf@42.student.fr>
2020-05-26 00:12:45 +0200

    add bonus1

---
+++ bonus1/source.c
@@ -0,0 +1,13 @@
+int main (int argc, char **argv) {
+    char buffer[64];
+    int i;
+
+    i = atoi(argv[1]);
+    if (i < 10) {
+        memcpy(buffer, argv[2], i * sizeof(int))
+        if (i == 1464814662) {
+            execl("/bin/sh");
+        }
+    }
+    return 0;
+}
