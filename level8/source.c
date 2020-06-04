2e9f06923302cf7f4a261dc34e212bb1a8e70865
adacher <adr.dacher@gmail.com>
2020-06-04 15:03:34 +0200

    almost done.

--- level8/source.c
+++ level8/source.c
@@ -4,32 +4,43 @@
 #include <sys/types.h>
 #include <stdio.h>
 
-int main (int argc, char **argv) {
+int main(void)
+{
     char buffer[64];
     char *dataAuth;
     char *dataService;
-    
-    while (1) {
+
+    while (1)
+    {
         printf("%p, %p", dataAuth, dataService);
-        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL)
+        {
             break;
         }
-        if (strncmp(buffer, "auth", 5) === 0) {
+        if (strncmp(buffer, "auth", 5) == 0)
+        {
             dataAuth = malloc(4);
-            if (strlen(buffer) + 5 < 0x1E) {
+            if (strlen(buffer) + 5 < 0x1E)
+            {
                 strcpy(dataAuth, buffer + 5);
             }
         }
-        if (strncmp(buffer, "reset", 5) == 0) {
+        if (strncmp(buffer, "reset", 5) == 0)
+        {
             free(dataAuth);
         }
-        if (strncmp(buffer, "service", 6) == 0) {
+        if (strncmp(buffer, "service", 6) == 0)
+        {
             dataService = strdup(buffer + 7);
         }
-        if (strncmp(buffer, "login", 5) == 0) {
-            if (*dataAuth + 32 != 0) {
+        if (strncmp(buffer, "login", 5) == 0)
+        {
+            if (*dataAuth + 32 != 0)
+            {
                 system("/bin/sh");
-            } else {
+            }
+            else
+            {
                 fwrite("Password:\n", 1, 10, stdout);
             }
         }
