2bcd8b6d8a61590f24abb09989132941b0089471
adacher <adr.dacher@gmail.com>
2020-06-04 15:04:45 +0200

    presque finit

--- bonus1/source.c
+++ bonus1/source.c
@@ -1,11 +1,14 @@
-int main (int argc, char **argv) {
-    char buffer[64];
+int main(int argc, char **argv)
+{
+    char buffer[40];
     int i;
 
     i = atoi(argv[1]);
-    if (i < 10) {
-        memcpy(buffer, argv[2], i * sizeof(int))
-        if (i == 1464814662) {
+    if (i < 10)
+    {
+        memcpy(buffer, argv[2], i * 4); // 40, 40 + 4, 48
+        if (i == 1464814662)
+        {
             execl("/bin/sh");
         }
     }
