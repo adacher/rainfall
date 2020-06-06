e4e58271ebb98f4280636ce18685805d66992e0d
adacher <adr.dacher@gmail.com>
2020-06-06 15:56:14 +0200

    plusque1

--- bonus1/source.c
+++ bonus1/source.c
@@ -4,9 +4,9 @@ int main(int argc, char **argv)
     int i;
 
     i = atoi(argv[1]);
-    if (i < 10)
+    if (i <= 9)
     {
-        memcpy(buffer, argv[2], i * 4); // 40, 40 + 4, 48
+        memcpy(buffer, argv[2], i * 4); // 40, 40 + 4, 44
         if (i == 1464814662)
         {
             execl("/bin/sh");
